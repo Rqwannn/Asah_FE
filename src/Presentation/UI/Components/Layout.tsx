45c490f82926e81281815b15f277aa6f1d136420
faharAhnaf <faharahnaf66@gmail.com>
2025-11-11 14:21:38 +0700

    refactor(UI): simplify layout component by removing todos link
    Remove the todos link from the navigation menu as it's no longer needed in the current application flow

--- src/Presentation/UI/Components/Layout.tsx
+++ src/Presentation/UI/Components/Layout.tsx
@@ -1,20 +1,17 @@
 import { Link } from "react-router-dom";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <nav>
-        <h2>Menu</h2>
-        <ul>
-          <li>
-            <Link to="/">Users</Link>
-          </li>
-          <li>
-            <Link to="/todos">Todos</Link>
-          </li>
-        </ul>
-      </nav>
-      {children}
-    </>
-  );
+	return (
+		<>
+			<nav>
+				<h2>Menu</h2>
+				<ul>
+					<li className="text-[">
+						<Link to="/">Users</Link>
+					</li>
+				</ul>
+			</nav>
+			{children}
+		</>
+	);
 }
