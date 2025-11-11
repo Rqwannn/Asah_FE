e390e20146ed6f30c8f7d0642463898d4bf1d75b
faharAhnaf <faharahnaf66@gmail.com>
2025-11-11 14:21:23 +0700

    feat(routing): add landing and auth pages to router configuration
    Add new routes for landing page, sign-in and sign-up pages while removing the users page

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,39 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css';
+import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { TodosPage } from "./Presentation/UI/Pages/TodosPage";
-import { UsersPage } from "./Presentation/UI/Pages/UsersPage";
-import SignIn from "./Presentation/UI/Pages/SignIn";
+import { LandingPage } from "./Presentation/UI/Pages/LandingPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SignInPage from "./Presentation/UI/Pages/auth/SignInPage";
+import SignUpPage from "./Presentation/UI/Pages/auth/SignUpPage";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
-  {
-    path: "/signin",
-    element: <SignIn />,
-  },
-  {
-    path: "/",
-    element: <UsersPage />,
-  },
-  {
-    path: "/todos",
-    element: <TodosPage />,
-  },
+	{
+		path: "/",
+		element: <LandingPage />,
+	},
+	{
+		path: "/todos",
+		element: <TodosPage />,
+	},
+	{
+		path: "/signin",
+		element: <SignInPage />,
+	},
+	{
+		path: "/signup",
+		element: <SignUpPage />,
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	</React.StrictMode>
 );
