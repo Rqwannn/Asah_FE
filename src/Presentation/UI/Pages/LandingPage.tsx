0143b80ce762769cfb744bd848e6ea53b1cfd2d7
faharAhnaf <faharahnaf66@gmail.com>
2025-11-11 14:21:45 +0700

    feat: add initial landing page component with layout

---
+++ src/Presentation/UI/Pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import { UserList } from "../../User/UserList";
+import { Layout } from "../Components/Layout";
+
+export function LandingPage() {
+	return (
+		<Layout>
+			<main>
+				<h1>Landing page</h1>
+				{/* <UserList /> */}
+			</main>
+		</Layout>
+	);
+}
