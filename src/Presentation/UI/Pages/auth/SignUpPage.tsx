ac0279cbcf2e5d9c519a0c9c794296919e412d72
faharAhnaf <faharahnaf66@gmail.com>
2025-11-11 14:22:02 +0700

    feat(auth): add sign up page component with basic layout

---
+++ src/Presentation/UI/Pages/auth/SignUpPage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SignUpPage = () => {
+	return (
+		<main className="min-h-screen bg-[#285F3E] w-full flex flex-col items-center justify-center">
+			<div className="w-[564px] bg-[#FFFFFF] h-[681px] rounded-xl">
+				<div className="py-14 px-8">
+					<div className="flex flex-col items-center justify-center">
+						<h1 className="text-[32px] font-bold pb-1">Create Your Account</h1>
+						<p className="text-[18px] font-normal">
+							Start learning and growing with us today.
+						</p>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
+};
+
+export default SignUpPage;
