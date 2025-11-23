d1a061331260d4eae95a86d67f1c24a9560e7b6c
faharAhnaf <faharahnaf66@gmail.com>
2025-11-23 23:53:48 +0700

    refactor(User): update user data source to use new models and DTOs
    Update import paths and model/DTO references to align with new project structure. Add new fields (skills and discovers) to the user model mapping.

--- src/Data/DataSources/User/UserAPIDataSource.ts
+++ src/Data/DataSources/User/UserAPIDataSource.ts
@@ -1,23 +1,24 @@
-import { UserDTO } from "../../../Data/DTOs/UserDTO";
-import { User } from "../../../Domain/Models/User";
+import { UserDTO } from "./../../DTOs/AuthDTO";
+import { UserModel } from "../../../Domain/Auth/Models/User";
 import { Http } from "../../../Services/Http";
 
 export interface UserDataSource {
-  getUsers(): Promise<User[]>;
+	getUsers(): Promise<UserModel[]>;
 }
 
 export class UserDataSourceImpl implements UserDataSource {
-  async getUsers(): Promise<User[]> {
-    const res = await Http.get<UserDTO[]>("/users");
-    return res.data.map((x) => this.mapToModel(x));
-  }
+	async getUsers(): Promise<UserModel[]> {
+		const res = await Http.get<UserDTO[]>("/users");
+		return res.data.map((x) => this.mapToModel(x));
+	}
 
-  mapToModel(dto: UserDTO): User {
-    return {
-      id: dto.id,
-      name: dto.name,
-      username: dto.username,
-      email: dto.email,
-    };
-  }
+	mapToModel(dto: UserDTO): UserModel {
+		return {
+			id: dto.id,
+			username: dto.username,
+			email: dto.email,
+			skills: dto.skills,
+			discovers: dto.discovers,
+		};
+	}
 }
