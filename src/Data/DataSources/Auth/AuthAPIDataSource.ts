8ec776a61c0ddfded4a2d69341dd5a3f8d27b27f
faharAhnaf <faharahnaf66@gmail.com>
2025-11-23 23:53:15 +0700

    feat(auth): implement auth API data source with signIn, signUp and logout methods

---
+++ src/Data/DataSources/Auth/AuthAPIDataSource.ts
@@ -0,0 +1,26 @@
+import {
+	SignInRequestDTO,
+	SignInResponseDTO,
+	SignUpRequestDTO,
+	SignUpResponseDTO,
+} from "@/Data/DTOs/AuthDTO";
+import { AuthDataSource } from "./AuthDataSource";
+import { Http } from "@/Services/Http";
+
+export class AuthAPIDataSource implements AuthDataSource {
+	async signIn(data: SignInRequestDTO): Promise<SignInResponseDTO> {
+		const res = await Http.post<SignInResponseDTO>("/auth/sign-in", data);
+		return res.data;
+	}
+
+	async signUp(data: SignUpRequestDTO): Promise<SignUpResponseDTO> {
+		const res = await Http.post<SignUpResponseDTO>("/auth/sign-up", data);
+		return res.data;
+	}
+
+	async logout(): Promise<void> {
+		localStorage.removeItem("accessToken");
+		localStorage.removeItem("user");
+		return;
+	}
+}
