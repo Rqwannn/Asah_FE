aa8fbce64da3940fe0d14d9b487b4c69407b044f
faharAhnaf <faharahnaf66@gmail.com>
2025-11-24 20:43:57 +0700

    feat(Navbar): add navigation links and logout functionality
    - Wrap navigation buttons in Link components for routing
    - Implement logout functionality using auth factory

--- src/Presentation/UI/Components/Navbar.tsx
+++ src/Presentation/UI/Components/Navbar.tsx
@@ -1,46 +1,65 @@
 import { Button } from "@/components/ui/button";
 import Icon from "@/assets/icon.svg";
+import { Link } from "react-router-dom";
+import { useAuthFactory } from "@/App/Factories/useAuthFactory";
 
 const Navbar = () => {
+	const { useLogout } = useAuthFactory();
+	const { logout } = useLogout();
+
+	const handleLogout = async () => {
+		await logout();
+	};
 	return (
 		<div className="w-[132px] h-[852px] bg-[#285F3E] flex flex-col rounded-xl absolute left-5 top-5 bottom-5 justify-between p-11">
 			<div className="">
 				<img src={Icon} alt="" />
 			</div>
 			<div className="flex flex-col items-center gap-16">
-				<Button
-					variant={null}
-					size={"icon-lg"}
-					className="text-[28px] text-[#FFFFFF] cursor-pointer">
-					<i className="ri-home-4-fill"></i>
-				</Button>
-				<Button
-					variant={null}
-					size={"icon-lg"}
-					className="text-[32px] text-[#FFFFFF] cursor-pointer ">
-					<i className="ri-graduation-cap-fill"></i>
-				</Button>
-				<Button
-					variant={null}
-					size={"icon-lg"}
-					className="text-[32px] text-[#FFFFFF] cursor-pointer">
-					<i className="ri-user-line"></i>
-				</Button>
-				<Button
-					variant={null}
-					size={"icon-lg"}
-					className="text-[32px] text-[#FFFFFF] cursor-pointer">
-					<i className="ri-message-3-fill"></i>
-				</Button>
-				<Button
-					variant={null}
-					size={"icon-lg"}
-					className="text-[32px] text-[#FFFFFF] cursor-pointer">
-					<i className="ri-settings-3-line"></i>
-				</Button>
+				<Link to={"/dashboard"}>
+					<Button
+						variant={null}
+						size={"icon-lg"}
+						className="text-[28px] text-[#FFFFFF] cursor-pointer">
+						<i className="ri-home-4-fill"></i>
+					</Button>
+				</Link>
+				<Link to={"/course"}>
+					<Button
+						variant={null}
+						size={"icon-lg"}
+						className="text-[32px] text-[#FFFFFF] cursor-pointer ">
+						<i className="ri-graduation-cap-fill"></i>
+					</Button>
+				</Link>
+				<Link to={"/"}>
+					<Button
+						variant={null}
+						size={"icon-lg"}
+						className="text-[32px] text-[#FFFFFF] cursor-pointer">
+						<i className="ri-user-line"></i>
+					</Button>
+				</Link>
+				<Link to={"/"}>
+					<Button
+						variant={null}
+						size={"icon-lg"}
+						className="text-[32px] text-[#FFFFFF] cursor-pointer">
+						<i className="ri-message-3-fill"></i>
+					</Button>
+				</Link>
+				<Link to={"/"}>
+					<Button
+						variant={null}
+						size={"icon-lg"}
+						className="text-[32px] text-[#FFFFFF] cursor-pointer">
+						<i className="ri-settings-3-line"></i>
+					</Button>
+				</Link>
 			</div>
 			<div className="flex flex-col items-center ">
 				<Button
+					onClick={handleLogout}
 					variant={"link"}
 					size={"icon-lg"}
 					className="text-[28px] text-[#FFFFFF] cursor-pointer">
