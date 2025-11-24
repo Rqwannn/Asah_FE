57086098ea17465f5455ac3af59ed29fd6693cc8
faharAhnaf <faharahnaf66@gmail.com>
2025-11-24 20:43:10 +0700

    feat(auth): add logout functionality to auth factory

--- src/App/Factories/useAuthFactory.ts
+++ src/App/Factories/useAuthFactory.ts
@@ -1,8 +1,9 @@
 import { AuthAPIDataSource } from "@/Data/DataSources/Auth/AuthAPIDataSource";
 import { AuthRepositoryImpl } from "@/Data/Repositories/AuthRepositoryImpl";
+import { LogoutUseCase } from "@/Domain/Auth/UseCases/LogoutUseCase";
 import { SignInUseCase } from "@/Domain/Auth/UseCases/SignInUseCase";
 import { SignUpUseCase } from "@/Domain/Auth/UseCases/SignUpUseCase";
-import { useSignIn, useSignUp } from "@/Presentation/Hooks/useAuth";
+import { useSignIn, useSignUp, useLogout } from "@/Presentation/Hooks/useAuth";
 
 export const useAuthFactory = () => {
 	const datasource = new AuthAPIDataSource();
@@ -11,5 +12,6 @@ export const useAuthFactory = () => {
 	return {
 		useSignIn: () => useSignIn(new SignInUseCase(repo)),
 		useSignUp: () => useSignUp(new SignUpUseCase(repo)),
+		useLogout: () => useLogout(new LogoutUseCase(repo)),
 	};
 };
