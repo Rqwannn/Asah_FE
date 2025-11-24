147124722977f0d8ed8b3788be96762622e5f536
faharAhnaf <faharahnaf66@gmail.com>
2025-11-24 15:32:02 +0700

    feat(ui): add navbar component with navigation buttons and logo

---
+++ src/Presentation/UI/Components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import Icon from "@/assets/icon.svg";
+
+const Navbar = () => {
+	return (
+		<div className="w-[132px] h-[852px] bg-[#285F3E] flex flex-col rounded-xl absolute left-5 top-5 bottom-5 justify-between p-11">
+			<div className="">
+				<img src={Icon} alt="" />
+			</div>
+			<div className="flex flex-col items-center gap-16">
+				<Button
+					variant={null}
+					size={"icon-lg"}
+					className="text-[28px] text-[#FFFFFF] cursor-pointer">
+					<i className="ri-home-4-fill"></i>
+				</Button>
+				<Button
+					variant={null}
+					size={"icon-lg"}
+					className="text-[32px] text-[#FFFFFF] cursor-pointer ">
+					<i className="ri-graduation-cap-fill"></i>
+				</Button>
+				<Button
+					variant={null}
+					size={"icon-lg"}
+					className="text-[32px] text-[#FFFFFF] cursor-pointer">
+					<i className="ri-user-line"></i>
+				</Button>
+				<Button
+					variant={null}
+					size={"icon-lg"}
+					className="text-[32px] text-[#FFFFFF] cursor-pointer">
+					<i className="ri-message-3-fill"></i>
+				</Button>
+				<Button
+					variant={null}
+					size={"icon-lg"}
+					className="text-[32px] text-[#FFFFFF] cursor-pointer">
+					<i className="ri-settings-3-line"></i>
+				</Button>
+			</div>
+			<div className="flex flex-col items-center ">
+				<Button
+					variant={"link"}
+					size={"icon-lg"}
+					className="text-[28px] text-[#FFFFFF] cursor-pointer">
+					<i className="ri-logout-box-r-line"></i>
+				</Button>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
