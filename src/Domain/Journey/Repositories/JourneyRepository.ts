ce869bcb665cfac66e2ef4ac3d5c555ff6c5b2f9
faharAhnaf <faharahnaf66@gmail.com>
2025-12-11 14:12:39 +0700

    feat(JourneyRepository): add getJourneyTrackings method to fetch journey tracking data

--- src/Domain/Journey/Repositories/JourneyRepository.ts
+++ src/Domain/Journey/Repositories/JourneyRepository.ts
@@ -1,6 +1,8 @@
 import { JourneyDTO } from "@/Data/DTOs/JourneyDTO";
+import { JourneyTrackingResponseDTO } from "@/Data/DTOs/JourneyTrackingDTO";
 
 export interface JourneyRepository {
-    getJourneys(): Promise<JourneyDTO[]>;
-    getJourney(id: string): Promise<JourneyDTO>;
+	getJourneys(): Promise<JourneyDTO[]>;
+	getJourney(id: string): Promise<JourneyDTO>;
+	getJourneyTrackings(id: string): Promise<JourneyTrackingResponseDTO[]>;
 }
