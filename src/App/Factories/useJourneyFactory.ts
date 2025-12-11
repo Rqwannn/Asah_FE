865eb08cd8a1c8f3f7984d4ae68d3cf3c1c22960
faharAhnaf <faharahnaf66@gmail.com>
2025-12-11 14:10:22 +0700

    feat(journey): add journey trackings factory function
    Add new factory function to create journey trackings hook instance

--- src/App/Factories/useJourneyFactory.ts
+++ src/App/Factories/useJourneyFactory.ts
@@ -1,17 +1,30 @@
 import { JourneyRepositoryImpl } from "@/Data/Repositories/JourneyRepositoryImpl";
 import { GetJourneyDetailUseCase } from "@/Domain/Journey/UseCases/GetJourneyDetailUseCase";
 import { GetJourneysUseCase } from "@/Domain/Journey/UseCases/GetJourneysUseCase";
-import { useJourneyDetail, useJourneys } from "@/Presentation/Hooks/useJourney";
+import { GetJourneyTrackingsUseCase } from "@/Domain/Journey/UseCases/GetJourneyTrackingsUseCase";
+import {
+	useJourneyDetail,
+	useJourneys,
+	useJourneyTrackings,
+} from "@/Presentation/Hooks/useJourney";
 import { axiosInstance } from "@/Services/Http";
 
 const journeyRepository = new JourneyRepositoryImpl(axiosInstance);
 const getJourneysUseCase = new GetJourneysUseCase(journeyRepository);
+
 const getJourneyDetailUseCase = new GetJourneyDetailUseCase(journeyRepository);
+const getJourneyTrackingsUseCase = new GetJourneyTrackingsUseCase(
+	journeyRepository
+);
 
 export const useJourneysFactory = () => {
-    return useJourneys(getJourneysUseCase);
+	return useJourneys(getJourneysUseCase);
 };
 
 export const useJourneyDetailFactory = (id: string) => {
-    return useJourneyDetail(getJourneyDetailUseCase, id);
+	return useJourneyDetail(getJourneyDetailUseCase, id);
+};
+
+export const useJourneyTrackingsFactory = (id: string) => {
+	return useJourneyTrackings(getJourneyTrackingsUseCase, id);
 };
