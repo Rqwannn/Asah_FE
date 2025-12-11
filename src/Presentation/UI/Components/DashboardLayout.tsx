c505ed654aa2b52130e719e603103cc3925917b2
faharAhnaf <faharahnaf66@gmail.com>
2025-12-11 14:13:53 +0700

    style(DashboardLayout): fix indentation and adjust responsive padding

--- src/Presentation/UI/Components/DashboardLayout.tsx
+++ src/Presentation/UI/Components/DashboardLayout.tsx
@@ -3,12 +3,12 @@ import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
-	return (
-		<div className="relative h-screen bg-[#F4F2EC]  lg:pl-[152px] overflow-hidden">
-			<Navbar />
-			<Outlet />
-		</div>
-	);
+  return (
+    <div className="relative h-screen overflow-hidden bg-[#F4F2EC] md:pl-[84px]">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 };
 
 export default DashboardLayout;
