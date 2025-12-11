615c1599ec8c7e328ba74847036cf31f1ff1dee6
faharAhnaf <faharahnaf66@gmail.com>
2025-12-11 14:12:08 +0700

    feat(JourneyRepository): add getJourneyTrackings method to fetch journey trackings

--- src/Data/Repositories/JourneyRepositoryImpl.ts
+++ src/Data/Repositories/JourneyRepositoryImpl.ts
@@ -1,18 +1,30 @@
 import { ApiResponseDTO } from "@/Data/DTOs/ApiResponseDTO";
 import { JourneyDTO } from "@/Data/DTOs/JourneyDTO";
+import { JourneyTrackingResponseDTO } from "@/Data/DTOs/JourneyTrackingDTO";
 import { JourneyRepository } from "@/Domain/Journey/Repositories/JourneyRepository";
 import { AxiosInstance } from "axios";
 
 export class JourneyRepositoryImpl implements JourneyRepository {
-    constructor(private http: AxiosInstance) {}
+	constructor(private http: AxiosInstance) {}
 
-    async getJourneys(): Promise<JourneyDTO[]> {
-        const response = await this.http.get<ApiResponseDTO<{ journeys: JourneyDTO[] }>>("/journeys");
-        return response.data.data.journeys;
-    }
+	async getJourneys(): Promise<JourneyDTO[]> {
+		const response = await this.http.get<
+			ApiResponseDTO<{ journeys: JourneyDTO[] }>
+		>("/journeys");
+		return response.data.data.journeys;
+	}
 
-    async getJourney(id: string): Promise<JourneyDTO> {
-        const response = await this.http.get<ApiResponseDTO<{ journey: JourneyDTO }>>(`/journeys/${id}`);
-        return response.data.data.journey;
-    }
+	async getJourney(id: string): Promise<JourneyDTO> {
+		const response = await this.http.get<
+			ApiResponseDTO<{ journey: JourneyDTO }>
+		>(`/journeys/${id}`);
+		return response.data.data.journey;
+	}
+
+	async getJourneyTrackings(id: string): Promise<JourneyTrackingResponseDTO[]> {
+		const response = await this.http.get<
+			ApiResponseDTO<JourneyTrackingResponseDTO[]>
+		>(`/journeys/${id}/tutorials/trackings`);
+		return response.data.data;
+	}
 }
