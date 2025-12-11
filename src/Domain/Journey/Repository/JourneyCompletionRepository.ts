ebec71441c5be671416d5ebf4049f7184f2856b1
faharAhnaf <faharahnaf66@gmail.com>
2025-12-11 14:12:47 +0700

    feat(JourneyCompletionRepository): add postCompletion method to interface
    Add method to allow posting journey completion data including rating and duration

--- src/Domain/Journey/Repository/JourneyCompletionRepository.ts
+++ src/Domain/Journey/Repository/JourneyCompletionRepository.ts
@@ -2,4 +2,9 @@ import { JourneyCompletionModel } from "../Models/JourneyCompletion";
 
 export interface JourneyCompletionRepository {
 	getCompletion(journeyId: string): Promise<JourneyCompletionModel | null>;
+	postCompletion(
+		journeyId: number,
+		rating: number,
+		duration: number
+	): Promise<JourneyCompletionModel>;
 }
