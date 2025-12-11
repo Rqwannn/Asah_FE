18c71b2ae94ca7eb6b1ce8258e156521626199a7
faharAhnaf <faharahnaf66@gmail.com>
2025-12-11 14:59:50 +0700

    feat(JourneyCompletionDataSource): add getUserCompletions method to fetch user journey completions

--- src/Data/DataSources/JourneyCompletionDataSource.ts
+++ src/Data/DataSources/JourneyCompletionDataSource.ts
@@ -1,29 +1,36 @@
 import { Http } from "@/Services/Http";
 import {
-	JourneyCompletionResponse,
-	PostJourneyCompletionResponse,
+  JourneyCompletionResponse,
+  PostJourneyCompletionResponse,
 } from "../DTOs/JourneyCompletionDTO";
 
 export class JourneyCompletionDataSource {
-	async getCompletion(journeyId: string): Promise<JourneyCompletionResponse> {
-		const response = await Http.get<JourneyCompletionResponse>(
-			`/journeys/${journeyId}/completions`
-		);
-		return response.data;
-	}
+  async getCompletion(journeyId: string): Promise<JourneyCompletionResponse> {
+    const response = await Http.get<JourneyCompletionResponse>(
+      `/journeys/${journeyId}/completions`,
+    );
+    return response.data;
+  }
 
-	async postCompletion(
-		journeyId: number,
-		rating: number,
-		duration: number
-	): Promise<PostJourneyCompletionResponse> {
-		const response = await Http.post<PostJourneyCompletionResponse>(
-			`/journeys/${journeyId}/completions`,
-			{
-				avg_submission_rating: rating,
-				study_duration: duration,
-			}
-		);
-		return response.data;
-	}
+  async getUserCompletions(): Promise<JourneyCompletionResponse> {
+    const response = await Http.get<JourneyCompletionResponse>(
+      `/journeys/completions`,
+    );
+    return response.data;
+  }
+
+  async postCompletion(
+    journeyId: number,
+    rating: number,
+    duration: number,
+  ): Promise<PostJourneyCompletionResponse> {
+    const response = await Http.post<PostJourneyCompletionResponse>(
+      `/journeys/${journeyId}/completions`,
+      {
+        avg_submission_rating: rating,
+        study_duration: duration,
+      },
+    );
+    return response.data;
+  }
 }
