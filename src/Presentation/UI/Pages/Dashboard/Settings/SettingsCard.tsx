1e186dd70cf39ffeaba0793af97881e3e3ecf334
faharAhnaf <faharahnaf66@gmail.com>
2025-12-11 17:55:23 +0700

    style(SettingsCard): adjust layout and fix indentation in component

--- src/Presentation/UI/Pages/Dashboard/Settings/SettingsCard.tsx
+++ src/Presentation/UI/Pages/Dashboard/Settings/SettingsCard.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 
 interface SettingsCardProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const SettingsCard = ({ children }: SettingsCardProps) => {
-	return (
-		<div className="relative mt-2"> 
-			<div className="w-[720px] h-[640px] bg-white border border-[#E7E6E0] rounded-[18px] shadow-[0_10px_24px_rgba(0,0,0,0.08)] overflow-hidden overflow-y-auto">
-				{children}
-			</div>
-		</div>
-	);
+  return (
+    <div className="h-[748px] w-full overflow-hidden overflow-y-auto rounded-[18px] border border-[#E7E6E0] bg-white shadow-[0_10px_24px_rgba(0,0,0,0.08)]">
+      {children}
+    </div>
+  );
 };
 
 export default SettingsCard;
